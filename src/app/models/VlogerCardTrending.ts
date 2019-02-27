d3983ccc89963d8664710fe230d1a0d9dd2073ab
tinashecee <40890317+tinashecee@users.noreply.github.com>
2019-02-27 15:32:42 +0200

    Add files via upload

---
+++ src/app/models/VlogerCardTrending.ts
@@ -0,0 +1,16 @@
+export
+interface VlogerCard{
+    thumb?:'',
+    title?:'',
+    vid?:'',
+    views?:number,
+    playlistId?:string,
+    subscribers?:number,
+    publishedAt?:any,
+    publishedDate?:any,
+    channelTitle?:'',
+    comment?:'',
+    tag?:string,
+    pic?:string
+
+}
---
+++ src/app/music/music.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {SeoService} from '../seo.service';
+import { Vloger} from '../models/Vloger';
+import {VlogerCard} from '../models/VlogerCardTrending';
+
+@Component({
+  selector: 'app-music',
+  templateUrl: './music.component.html',
+  styleUrls: ['./music.component.css']
+})
+export class MusicComponent implements OnInit {
+  artists:Vloger[];
+  constructor(private service:SeoService) {
+    this.service.data12.subscribe(v=>{
+      this.artists=v
+      console.log(this.artists)
+    });
+   }
+
+  ngOnInit() {
+
+  }
+  loadPlaylist(v:Vloger) {
+    let vv:VlogerCard={
+      playlistId:v.playlistId,
+      pic:v.pic
+    }
+    this.service.getVlogers(v);
+  }
+}
